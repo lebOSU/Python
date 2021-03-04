880cc79fdbd31ed5f39a63f02e0fb9d941eb4f4d
lebOSU <caleb.j.russell@okstate.edu>
2021-03-03 23:59:15 -0600

    homework added

---
+++ Mod05Homework.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#Caleb Russell
+#Mod 05 Homework
+
+clear
+echo "Welcome to the soda machine.	You can enter values of 5, 10 or 25 in payment."
+read -p "What type of soda would you like? " soda_choice
+echo $soda_choice
+echo
