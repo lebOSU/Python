1df2d7f2a7a3a8df399203b78e2f5409586d7150
lebOSU <caleb.j.russell@okstate.edu>
2021-03-31 09:43:04 -0500

    p2.sh rough draft 9:42AM

--- p2.sh
+++ p2.sh
@@ -2,18 +2,54 @@
 
 major_descriptors="BAD-TRAFFIC,DNS SPOOF,ET CURRENT_EVENTS,ET DNS,ET INFO,ET MALWARE,ET POLICY,ET TROJAN,ET WEB_CLIENT,ICMP,INFO,SCAN,WEB-IIS"
 
+#Code order
+#	DONE	Shebang #! /bin/sh
+#	DONE	Descriptor string
+#	Menu choice 1 function
+#	Menu choice 2 function
+#	Menu choice 3 function
+#	Menu choice 4 function
+
+#Menu choice 1 function		Parse alert data
+menu_choice_1 () {
+
+}
+
+#Menu choice 2 function		Major descriptors
+menu_choice_2 () {
+
+}
+
+#Menu choice 3 function		Classifications
+menu_choice_3 () {
+echo "This is hateful to do in bash.	Hard pass."
+}
+
+#Menu choice 4 function		Clean up and exit
+menu_choice_4 () {
+
+}
+
 
 #Main code body:
-	#Create major descriptors array
-		#major_descriptors_array = ( $major_descriptors )
-	#Name strings
-		#MY_NAME="Caleb Russell"
-	#Data file check
-	#Menu
-		#1. Parse alert data
-		#2. Major descriptors
-		#3. Classifications
-		#4. Clean up and exit
+#	Create major descriptors array
+#		major_descriptors_array = ( $major_descriptors )
+#		User input allows for spaces
+#		Array and clean data use _underscore. not space
+#	Name strings
+#		MY_NAME="Caleb Russell"
+#		Split that variable to make a variable for last name and for first name
+#		Use these variables when making the final .tgz file
+#	Data file check
+#	Menu
+#		use a single echo statement, then IF/ELIF/ELSE for options
+#		catch invalid options and have the user try again
+
+#Name strings
+
+
+#Data file check
+
 
 #Menu
 #-----MAIN MENU-----
