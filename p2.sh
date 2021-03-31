1ca93a5d2f7abf64883d62ab697616d2ff79da1f
lebOSU <caleb.j.russell@okstate.edu>
2021-03-31 17:04:58 -0500

    project 2 draft 5:04pm

--- p2.sh
+++ p2.sh
@@ -5,7 +5,7 @@
 #	past solutions
 #	Burkman's demo video
 #	To-do list .docx
-
+clear
 #Descriptor string
 major_descriptors="BAD-TRAFFIC,DNS SPOOF,ET CURRENT_EVENTS,ET DNS,ET INFO,ET MALWARE,ET POLICY,ET TROJAN,ET WEB_CLIENT,ICMP,INFO,SCAN,WEB-IIS"
 
@@ -17,32 +17,35 @@ major_descriptors="BAD-TRAFFIC,DNS SPOOF,ET CURRENT_EVENTS,ET DNS,ET INFO,ET MAL
 #	Menu choice 3 function
 #	Menu choice 4 function
 
-#Menu choice 1 function		Parse alert data
+#Menu choice 1 function			Parse alert data
 menu_choice_1 () {
 true;
+clear
+echo "Please be patient. Parsing data..."
 }
 
-#Menu choice 2 function		Major descriptors
+#Menu choice 2 function			Major descriptors
 menu_choice_2 () {
 true;
+clear
 }
 
-#Menu choice 3 function		Classifications
+#Menu choice 3 function			Classifications
 menu_choice_3 () {
-echo "This is hateful to do in bash.	Hard pass."
+clear
+echo "This is hateful to do in bash. Hard pass."
 }
 
-#Menu choice 4 function		Clean up and exit
+
+#Menu choice 4 function			Clean up and exit
 menu_choice_4 () {
 true;
+clear
 }
 
 
 #Main code body:
 #	Create major descriptors array
-#		major_descriptors_array = ( $major_descriptors )
-#		User input allows for spaces
-#		Array and clean data use _underscore. not space
 #	Name strings
 #		MY_NAME="Caleb Russell"
 #		Split that variable to make a variable for last name and for first name
@@ -52,6 +55,14 @@ true;
 #		use a single echo statement, then IF/ELIF/ELSE for options
 #		catch invalid options and have the user try again
 
+#Create major descriptors array
+declare -a major_descriptors_array=$(echo $major_descriptors | "," " ")
+for i in ${major_descriptors_array[*]};
+	do
+		echo $i
+	done
+
+
 #Name strings
 MY_NAME="Caleb Russell"
 first_name=$(echo $MY_NAME | cut -d " " -f 1)
@@ -59,14 +70,36 @@ last_name=$(echo $MY_NAME | cut -d " " -f2)
 echo $first_name
 echo $last_name
 
+
 #Data file check
 
 
 #Menu
-#-----MAIN MENU-----
-#Please select from the following options...
-#
-#1.	Parse alert data
-#2.	Major descriptors
-#3.	Classifications
-#4.	Clean up and exit	
+while true;
+do
+	echo "
+-----MAIN MENU-----
+Please select from the following options...
+
+1.	Parse alert data
+2.	Major descriptors
+3.	Classifications
+4.	Clean up and exit	
+"
+
+read -p "Option #: " user_menu_choice
+if [[ $user_menu_choice -eq 1 ]];then
+	menu_choice_1
+elif [[ $user_menu_choice -eq 2 ]];then
+	menu_choice_2
+elif [[ $user_menu_choice -eq 3 ]];then
+	menu_choice_3
+elif [[ $user_menu_choice -eq 4 ]];then
+	menu_choice_4
+else
+	clear
+	echo "That is not a valid option.	Please try again."
+	continue
+fi
+
+done
