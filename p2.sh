265f16cf903c05f6e6d9091dc84cf7b0a0bc5d8b
lebOSU <caleb.j.russell@okstate.edu>
2021-03-31 08:55:41 -0500

    project 2 rough draft

---
+++ p2.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+major_descriptors="BAD-TRAFFIC,DNS SPOOF,ET CURRENT_EVENTS,ET DNS,ET INFO,ET MALWARE,ET POLICY,ET TROJAN,ET WEB_CLIENT,ICMP,INFO,SCAN,WEB-IIS"
+
+
+#Main code body:
+	#Create major descriptors array
+		#major_descriptors_array = ( $major_descriptors )
+	#Name strings
+		#MY_NAME="Caleb Russell"
+	#Data file check
+	#Menu
+		#1. Parse alert data
+		#2. Major descriptors
+		#3. Classifications
+		#4. Clean up and exit
+
+#Menu
+#-----MAIN MENU-----
+#Please select from the following options...
+#
+#1.	Parse alert data
+#2.	Major descriptors
+#3.	Classifications
+#4.	Clean up and exit	
